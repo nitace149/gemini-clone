4f14fe08ac50a2b8790335d929f411f400ac3a24
Nitesh <niteshtyagi149@gmail.com>
2025-07-29 03:34:09 +0530

    UI complete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar.jsx';
+import Main from './components/Main/MainS.jsx';
+
+
+
+const App = () => {
+
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  )
+}
+
+export default App;
